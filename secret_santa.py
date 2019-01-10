eadee583980a20ddb5df61d03a0e1a4318dab876
sparth99 <sparth99@gmail.com>
2019-01-10 17:35:51 -0600

    csv feature

--- secret_santa.py
+++ secret_santa.py
@@ -4,6 +4,8 @@ from amazon.api import AmazonAPI
 
 game_spending_limit = 20
 
+import csv
+
 class Player:
 
 	def __init__(self,name, email, wish_list):
@@ -12,6 +14,24 @@ class Player:
 		self.wish_list = wish_list
 		self.other_player = None
 
+def read_csv():
+	players = list()
+	with open('yea.csv') as csv_file:
+	    csv_reader = csv.reader(csv_file, delimiter=',')
+	    line_count = 0
+	    for row in csv_reader:
+	        if line_count == 0:
+	            line_count += 1
+	        else:
+	        	name = row[0]
+	        	email = row[1]
+	        	wish_list = row[2]
+	        	line_count = line_count + 1
+	        	temp = Player(name, email, wish_list)
+	        	players.append(temp)
+
+
+	return players
 
 def assign_players(list1):
 	temp_list = list1.copy()
@@ -22,7 +42,6 @@ def assign_players(list1):
 			random_num = randint(0,len(temp_list) - 1)
 			# special case prevent infinite loop. if only one person left. 
 			if(x.name == temp_list[random_num].name and len(temp_list) == 1):
-				print("Hello")
 				rand = randint(0,len(list1)-1)
 				if list1[rand].other_player is not None:
 					list1[rand].other_player = temp_list[0]
@@ -40,30 +59,18 @@ def assign_players(list1):
 def send_mail(player):
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
-	server.login("your email", "your password")
+	server.login("email", "pass")
 	wish_list = player.other_player.wish_list.split(" ")
 	print(wish_list)
 	msg = "Your Pairing is " + player.other_player.name + "! \n " + "Here is his Wish-List: \n" + ' '.join(wish_list)
-	server.sendmail("your email", player.email , msg)
+	server.sendmail("email", player.email , msg)
 	server.quit()
 
 def execute_secret_santa(final_list):
 	for x in final_list:
 		send_mail(x)
+	print("\nEmails have been sent")
 
-all_players = [];
-
-Shivam = Player("Shivam",'email',"Socks HeadPhones Clothes")
-Parth = Player("Parth", "email","UT-Gear Swag")
-Ankush = Player("Ankush", "email", "USC-gear")
-Gaurav = Player("Gaurav","@gmail.com", "Cool-Books Apple-Watch")
-Yash = Player("Yash", "@gmail.com","Sweats Joggers")
-
-all_players.append(Shivam)
-all_players.append(Parth)
-all_players.append(Ankush)
-all_players.append(Gaurav)
-all_players.append(Yash)
 
 AMAZON_ACCESS_KEY = ""
 AMAZON_SECRET_KEY = ""
@@ -71,8 +78,7 @@ AMAZON_ASSOC_TAG = ""
 
 
 
-# for x in all_players:
-# 	print(all_players.email)
+all_players = read_csv()
 
 final_list = assign_players(all_players)
 for x in final_list:
@@ -80,3 +86,5 @@ for x in final_list:
 	print(x.other_player.name)
 	print("----------------------")
 execute_secret_santa(final_list)
+
+
